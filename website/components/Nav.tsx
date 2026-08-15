504cc14a1224a14aaa19029cb63cd41140a4df15
Carter Smotherman <56828535+csmotherman@users.noreply.github.com>
2026-08-15 16:31:22 -0400

    Add pilot website navigation

---
+++ website/components/Nav.tsx
@@ -0,0 +1,14 @@
+import Link from "next/link";
+
+export function Nav(){
+  return <nav className="nav">
+    <strong>CFB Analytics Pilot</strong>
+    <Link href="/">Home</Link>
+    <Link href="/teams">Teams</Link>
+    <Link href="/rankings">Rankings</Link>
+    <Link href="/compare">Compare</Link>
+    <Link href="/simulator">Simulator</Link>
+    <Link href="/archetypes">Archetypes</Link>
+    <Link href="/metrics">Metrics</Link>
+  </nav>;
+}
