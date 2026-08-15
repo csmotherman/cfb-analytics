2f4ad817cb95d97acfc7acad567ab653f7d14b79
Carter Smotherman <56828535+csmotherman@users.noreply.github.com>
2026-08-14 21:58:32 -0400

    Export archetype catalog and historical matcher

--- src/cfb_analytics/profiles/__init__.py
+++ src/cfb_analytics/profiles/__init__.py
@@ -5,15 +5,20 @@ season-relative grades, historical fingerprints, and explainable archetypes.
 """
 
 from .archetypes import classify_archetypes
+from .archetype_catalog import CATALOG, CATALOG_VERSION
 from .contract import PROFILE_METRICS, PROFILE_VERSION
 from .grades import grade_percentile, percentile_rank
+from .match_archetypes import match_snapshot
 from .similarity import historical_comparables
 
 __all__ = [
+    "CATALOG",
+    "CATALOG_VERSION",
     "PROFILE_METRICS",
     "PROFILE_VERSION",
     "classify_archetypes",
     "grade_percentile",
     "historical_comparables",
+    "match_snapshot",
     "percentile_rank",
 ]
