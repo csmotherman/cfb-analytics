21a7be7e915ebae346e077e80c69adf091ff67f6
Carter Smotherman <56828535+csmotherman@users.noreply.github.com>
2026-08-20 17:23:21 -0400

    Remove analytics subnav from section layout

--- website/app/analytics/layout.tsx
+++ website/app/analytics/layout.tsx
@@ -1,11 +1,9 @@
 import type {ReactNode} from "react";
-import {AnalyticsNav} from "../../components/AnalyticsNav";
 import "../../styles/analytics-shell.css";
 
 export default function AnalyticsLayout({children}:{children:ReactNode}){
   return <div className="analytics-shell">
     <div className="wrap analytics-shell-inner">
-      <AnalyticsNav/>
       {children}
     </div>
   </div>;
